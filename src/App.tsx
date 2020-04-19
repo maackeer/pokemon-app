b333a0156be3544f67b85014dab7f358c0346b31
Jonathan Peraza Feliciano <jonathan.peraza@wizeline.com>
2020-04-19 18:37:22 -0500

    feat: cambiar estructura para usar card de pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PokemonCard from './components/pokemonCard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokemon App</h1>
+      <PokemonCard 
+        imagen="01.jpg"
+        numero={1}
+        nombre="bulbasaur"
+        altura={1.5}
+        peso={55}
+      />
     </div>
   );
 }
