e377fbb12705f4132a60a0d5e3b29b15d7e74fb6
maackeer <salvador.garcia.pech@gmail.com>
2020-04-19 19:26:05 -0500

    agregar datos ejemplo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import PokemonCard from './components/pokemonCard';
+import data from './components/PokemonData';
+
+const getPokemonData = () => {
+  return {
+    imagen: data.sprites.front_default,
+    numero: data.id,
+    nombre: data.name,
+    altura: data.height,
+    peso: data.weight,
+  }
+}
 
 const App = () => {
+  const pokemon = getPokemonData();
+  const { imagen, numero, nombre, altura, peso } = pokemon;
+
   return (
     <div>
       <h1>Pokemon App</h1>
       <PokemonCard 
-        imagen="01.jpg"
-        numero={1}
-        nombre="bulbasaur"
-        altura={1.5}
-        peso={55}
+        imagen={imagen}
+        numero={numero}
+        nombre={nombre}
+        altura={altura}
+        peso={peso}
       />
     </div>
   );
